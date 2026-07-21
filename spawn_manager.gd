6053df03d9eb81d021d6dc90e0e48646161fcf9d
RedTomDev <LizzyRizzy@proton.me>
2026-07-20 17:54:11 -0700

    The Detective Commit
    Added in Detective Chair Phelps model and have added script for spawning him in and infrastrcture to spawn other chairs into the scene as well as a folder to maintain them. Have also added a script allowing player movement.

---
+++ spawn_manager.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+var detective_chair_phelps = preload("res://chairs/detective_chair_phelps_characterbody.tscn")
+var node_to_load
+
+func _ready() -> void:
+	node_to_load = detective_chair_phelps.instantiate()
+	add_child(node_to_load)
+	node_to_load.position.y -= 3
